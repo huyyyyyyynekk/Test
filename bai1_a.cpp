67bb7d32fcc156c27248fb8a19d427935424df82
huyyyyyyynekk <152782590+huyyyyyyynekk@users.noreply.github.com>
2024-07-28 19:48:47 +0700

    Add files via upload

---
+++ bai1_a.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b;
+    char pt;
+
+    printf("Nhap vao hai so nguyen duong a va b: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Nhap phep toan (+, -, *, /, ^): ");
+    scanf(" %c", &pt);
+
+    
+    switch (pt) {
+        case '+':
+            printf("%d + %d = %d\n", a, b, a + b);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", a, b, a - b);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", a, b, a * b);
+            break;
+        case '/':
+            if (b != 0) {
+                printf("%d / %d = %.2f\n", a, b, (float)a / b);
+            } else {
+                printf("Loi: Khong chia duoc cho 0\n");
+            }
+            break;
+        case '^':
+            printf("%d ^ %d = %.2f\n", a, b, pow(a, b));
+            break;
+        default:
+            printf("Loi: Phep toan khong hop le\n");
+    }
+
+    return 0;
+}
+
